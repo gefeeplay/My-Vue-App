88a9564352f057e288971ad6999d3846a3a4ccdd
Molyarov <megamisha41521@gmail.com>
2025-07-15 16:37:06 +0300

    vue HabitForm vers 1.0

---
+++ src/components/HabitForm.vue
@@ -0,0 +1,50 @@
+<script setup>
+import {computed, reactive, ref, watch} from 'vue'
+const habit = reactive({});
+const sum = computed(() => habit.frequency * habit.count);
+const label = ref();
+/*
+watch(() => {
+    return {name: habit.name, description: habit.description}
+}, (newValue) => { 
+    label.value = `${newValue?.name} ${newValue?.description}`
+})*/
+</script>
+
+<template>
+      <div class="container">
+        <form onsubmit="sumbitButton()">
+            <h1>Редактирование привычки</h1>
+            {{ label }}
+            <div class="form-group">
+                <label for="habitName">Название привычки</label>
+                <input v-model.trim="habit.name" id="habitName" name="habitName" required>
+            </div>
+        </br>
+            <div>
+                <label for="descryption">Описание привыки
+                </label>
+                <textarea v-model.trim="habit.description" id="descryption"></textarea>
+            </div>
+        </br>    
+            <div><label for="frequency">Частота выполнения
+                </label>
+                <select v-model="habit.frequency" id="frequency">
+                    <option value="1">Каждый день</option>
+                    <option value="7">Каждую неделю</option>
+                    <option value="30">Каждый месяц</option>
+                </select>
+            </div>
+        </br>            
+            <div>
+                <label for="count">Количество повторений</label>
+                <input v-model.number="habit.count" id="count" type="number"></input>
+            </div>
+        </br>    
+            <div id="SumElement">Общее количество выполнения: <span id="sum">{{ sum }}</span></div>
+            </br>
+            <button type="submit">Сохранить</button>
+        </form>
+    </div>
+</template>
+
